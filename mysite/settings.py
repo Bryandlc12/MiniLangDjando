ca74a44df7a1cf62c13373c3701755f2d7027bf0
Bryandlc12 <bryan3003delacruz@gmail.com>
2025-10-21 15:24:19 -0600

    Configurar staticfiles y collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -27,6 +28,9 @@ DEBUG = True
 STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 # Application definition
